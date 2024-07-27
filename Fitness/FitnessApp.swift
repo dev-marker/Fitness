5c42bd2aedeb47d23ad28913d576bd995ab4d8ef
Dev Marker <fistondev@tuta.io>
2024-07-27 11:27:48 +0200

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Fiston Niyonkuru on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
